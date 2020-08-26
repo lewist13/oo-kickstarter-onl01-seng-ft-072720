7e0e41d47639df0a060c49d966e84ae73c51277d
TaeVon Lewis <lewistpro@gmail.com>
2020-08-26 17:02:18 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,13 @@
 class Project
-
-  attr_accessor :backers, :title
+  attr_accessor :title, :backers
 
   def initialize(title)
-    @backers = []
     @title = title
+    @backers = []
   end
 
   def add_backer(backer)
     @backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
   end
-
 end

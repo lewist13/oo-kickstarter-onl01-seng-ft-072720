2912c408455432c545ce49e4fd5a2638cec47334
TaeVon Lewis <lewistpro@gmail.com>
2020-08-26 16:54:58 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,15 @@
 bob = Backer.new("Bob")
 awesome_project = Project.new("This is an Awesome Project")
- 
+
 bob.back_project(awesome_project)
- 
+
 bob.backed_projects
 # => #<Project:0x000001018683d0 @title="This is an Awesome Project"...>
- 
+
 awesome_project.backers
 
-class Backer 
+class Backer
 end
 
-class Project 
+class Project
 end

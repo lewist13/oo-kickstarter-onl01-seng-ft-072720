68917da7869240bc452f617e8833a315bbd89a90
TaeVon Lewis <lewistpro@gmail.com>
2020-08-26 16:59:01 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,13 @@
-bob = Backer.new("Bob")
-awesome_project = Project.new("This is an Awesome Project")
-
-bob.back_project(awesome_project)
-
-bob.backed_projects
-# => #<Project:0x000001018683d0 @title="This is an Awesome Project"...>
-
-awesome_project.backers
-
 class Backer
-end
 
-class Project
+  attr_accessor :backed_projects, :name
+  def initialize(name)
+    @backed_projects = []
+    @name = name
+  end
+
+  def back_project(project)
+    @backed_projects << project
+  end
+
 end
